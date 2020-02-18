daa8df3b09708b409de073b2b01724239afdce22
dfkeenan <dfkeenan@gmail.com>
2020-02-18 21:25:35 +1000

    Changed stuff.

--- Source/Game.gd
+++ Source/Game.gd
@@ -4,11 +4,12 @@ extends Node
 # var a = 2
 # var b = "text"
 
+onready var _scene_transition : TransitionLayer = $SceneTransition
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	SceneTransition.default_transition = load("res://core/transitions/assets/alien_transition.tres")
-	SceneTransition.transition_in(null)
-	pass # Replace with function body.
+	_scene_transition.transition_in(null)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -16,7 +17,7 @@ func _ready():
 
 func _unhandled_input(event):
 	if Input.is_key_pressed(KEY_R):
-		SceneTransition.transition_out(null)
-		yield(SceneTransition,"transition_complete")
+		_scene_transition.transition_out(null)
+		yield(_scene_transition,"transition_complete")
 		yield(get_tree().create_timer(1),"timeout")
 		get_tree().reload_current_scene()
--- Source/ui/HUD.gd
+++ Source/ui/HUD.gd
@@ -7,7 +7,6 @@ onready var _inventory_container : Control = $CenterContainer/InventoryContainer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_inventory_container.visible = false
-	pass # Replace with function body.
 
 func _unhandled_input(event):
 	if event.is_action_pressed("toggle_inventory"):
